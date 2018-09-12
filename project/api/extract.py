456251c1bc79adab4abb916865dc5e6bb33dd33a
Lucas Dutra <ldutra98@gmail.com>
2018-09-12 00:52:44 -0300

    Describe POST method behavior with Flask patterns
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- project/api/extract.py
+++ project/api/extract.py
@@ -1,7 +1,21 @@
 from flask import Flask, request
+import pytesseract
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/extract', method['POST'])
+@app.route('/extract', methods=['POST'])
 def extract():
     if(request.method == 'POST'):
-        if
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
