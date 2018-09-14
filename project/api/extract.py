d5f943392b0c30fa249acdb8eacc4227f9f2be55
Dutra <ldutra98@gmail.com>
2018-09-14 12:04:04 -0300

    Add jsonify to return statements
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- project/api/extract.py
+++ project/api/extract.py
@@ -1,6 +1,6 @@
 import pytesseract
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from project import app
 from pdf2image import convert_from_bytes
@@ -20,15 +20,30 @@ def allowed_file(filename):
 def extract():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return 'File not found'
+            return jsonify({
+                'message': 'File not found',
+                'code': 400
+            })
         file = request.files['file']
         if file.filename == '':
-            return 'Empty file'
+            return jsonify({
+                'message': 'File not sent', 
+                'code': 400
+            })
         if not allowed_file(file.filename):
-            return 'Wrong extension'
+            return jsonify({
+                'message': 'Not allowed file extension',
+                'code': 400
+            })
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             convert_pdf = convert_from_bytes(file.read())
             for page in convert_pdf:
                 print(pytesseract.image_to_string(page, lang='por'))
-            return file
+            return jsonify({
+                'message': 'Success',
+                'code': 200
+            })
+
+#passar returns pra json
+#desacoplar código 
