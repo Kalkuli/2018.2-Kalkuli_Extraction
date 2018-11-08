d08b67f93e9f0524f4d6b7774bd3e8790588c509
EsioFreitas <esio.gustavo@gmail.com>
2018-11-08 19:41:31 -0200

    Create class for handling S3 interaction
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -6,9 +6,8 @@ from werkzeug.utils import secure_filename
 from project import app
 from PIL import Image
 from celery import shared_task
-import boto3
-import botocore
 from project.api.extraction import Extraction
+from project.api.s3_utils import S3Utils
 
 
 UPLOAD_FOLDER = os.path.relpath('./project/assets')
@@ -16,22 +15,10 @@ ALLOWED_EXTENSIONS = set(['pdf'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-def get_s3_instance():
-    return boto3.client(
-        "s3",
-        aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
-        aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY")
-    )
-
-
 @shared_task
 def extraction_task(filename):
-    s3 = get_s3_instance()
-    response = s3.get_object(
-        Bucket=os.environ.get("S3_BUCKET_NAME"),
-        Key=filename
-    )
+    s3 = S3Utils()
+    response = s3.get_file(filename)
     file = response['Body'].read()
     extraction = Extraction(file)
     json_text = extraction.extract()
@@ -40,24 +27,9 @@ def extraction_task(filename):
         "filename": filename
     }
 
-
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def upload_to_s3(file):
-    s3 = get_s3_instance()
-
-    try:
-        s3.upload_fileobj(
-            file,
-            os.environ.get("S3_BUCKET_NAME"),
-            file.filename
-        )
-    except Exception as e:
-        return e
-
 
 @app.route('/extract', methods=['POST'])
 def extract():
@@ -74,11 +46,11 @@ def extract():
         }), 415
 
     filename = secure_filename(file.filename)
-    upload_to_s3(file)
+    s3 = S3Utils()
+    s3.upload_to_s3(file)
     task = extraction_task.delay(filename)
     return jsonify({'location': url_for('get_status',
                                         task_id=task.id)}), 202
-
 
 @app.route('/status_extraction/<task_id>')
 def get_status(task_id):
